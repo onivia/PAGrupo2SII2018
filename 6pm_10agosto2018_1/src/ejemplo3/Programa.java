0d391eeab9eb39582e82b3f2684cbac6b62ae566
onivia <ohnivia@gmail.com>
2018-08-13 19:25:09 -0500

    ciclos while y for.

---
+++ 6pm_10agosto2018_1/src/ejemplo3/Programa.java
@@ -0,0 +1,17 @@
+package ejemplo3;
+
+public class Programa {
+    public static void main(String[] args) {
+        
+        int i = 2;        
+        while(i <= 4) {            
+            System.out.println(i);            
+            
+            i++;
+        }             
+        
+        for (i = 2; i < 5; i++) {
+            System.out.println(i);                        
+        }
+    }    
+}
