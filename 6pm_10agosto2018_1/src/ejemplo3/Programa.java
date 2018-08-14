8e4f1d662e60deda85bd71ed509361f0ec58abd7
onivia <ohnivia@gmail.com>
2018-08-13 19:34:03 -0500

    ejercicio muestra impares desdentemente de 10 a 1.

--- 6pm_10agosto2018_1/src/ejemplo3/Programa.java
+++ 6pm_10agosto2018_1/src/ejemplo3/Programa.java
@@ -3,15 +3,12 @@ package ejemplo3;
 public class Programa {
     public static void main(String[] args) {
         
-        int i = 2;        
-        while(i <= 4) {            
-            System.out.println(i);            
-            
-            i++;
-        }             
-        
-        for (i = 2; i < 5; i++) {
-            System.out.println(i);                        
+        int i = 10;
+        while(i>=1) {
+            if((i%2) != 0) {
+                System.out.println(i);
+            }
+            i--;
         }
     }    
 }
