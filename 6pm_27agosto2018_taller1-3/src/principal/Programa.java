56adaf5bf7021c3947707bf2b6b06a2f16b44909
onivia <ohnivia@gmail.com>
2018-08-31 19:56:46 -0500

    item 6 resuelto, taller.

--- 6pm_27agosto2018_taller1-3/src/principal/Programa.java
+++ 6pm_27agosto2018_taller1-3/src/principal/Programa.java
@@ -1,12 +1,15 @@
 package principal;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Programa {
     public static void main(String[] args) {
         //item1();
         //item2();        
-        item5();
+        //item5();
+        //item6();
+        item7();
     }
     
     public static void item1() {
@@ -73,6 +76,122 @@ public class Programa {
             System.out.println("NO hay repetidos o INDETERMINADO");
         
         //ordenaAscendentemente(nums);
+    }
+    
+    public static void item6() {
+        int idesde = 3;
+        int ihasta = 7;
+        
+        int aleatorio = (new Random()).nextInt((ihasta - idesde) + 1);
+        int numeroEnRango = idesde + aleatorio;
+        System.out.println(numeroEnRango);
+        
+        //[3-7]
+        //R(5) -> r1
+        //(idesde + r1)
+    }
+    
+    public static void item7() {
+        /*
+        {"a","e","i","o","u","b","c","d","f","g","0","1","2,"3","4","_","*","+","%"}
+        
+        //getSimbolo(0,15):String
+        //validaRepeticion(int[] arreglo, string simbolo, int cantMaxRepeticiones):int-->cantidad de repeticiones
+        //validaRepeticionContrasena(int[] contrasenas):boolean-->true-valido; false-hay al menos una repetida        
+        */
+        
+        String[] contrasenas = new String[5];
+        
+        boolean alMenosHayUnaRepetida = true;
+        while(alMenosHayUnaRepetida) {
+            generaContrasenas(contrasenas);            
+            alMenosHayUnaRepetida = validaRepeticionContrasena(contrasenas);
+        }
+        
+        System.out.println(Arrays.toString(contrasenas));
+    }
+    
+    private static void generaContrasenas(String[] contrasenas) {
+        String[] contrasena = new String[5];
+        boolean esValidoSimbolo = false;
+        
+        int i = 0;
+        while(i < 5) {
+            esValidoSimbolo = false;
+            
+            generaContrasena(contrasena);
+            
+            for (int j = 0; j < 5; j++) {
+                esValidoSimbolo = validaRepeticion(contrasena, contrasena[j], 2);
+                
+                if(!esValidoSimbolo)
+                    break;
+            }
+            
+            if(esValidoSimbolo) {
+                contrasenas[i] = Arrays.toString(contrasena);
+                ++i;
+            }
+        }                
+    }
+    
+    private static void generaContrasena(String[] contrasena) {
+        for (int i = 0; i < contrasena.length; i++) {
+            contrasena[0] = getSimbolo(0, 15);
+            for (int j = 1; j < contrasena.length; j++) {
+                contrasena[j] = getSimbolo(0, 15);
+            }
+        }
+    }
+    
+    private static boolean validaRepeticionContrasena(String[] contrasenas) {
+        //{"a","b","x","c"}
+        boolean huboAlmenosUnaRepetida = false;
+        for (int i = 0; i < contrasenas.length -1; i++) {
+            for (int j = (i+1); j < contrasenas.length; j++) {
+                if(contrasenas[i].equals(contrasenas[j])) {
+                    huboAlmenosUnaRepetida = true;
+                    break;                    
+                }                                
+            }
+            if(huboAlmenosUnaRepetida)
+                break;
+        }
+        
+        return huboAlmenosUnaRepetida;
+    }
+    
+    private static boolean validaRepeticion(String[] arreglo, String simbolo, int cantMaxRepeticiones) {
+        boolean esValidaContrasena = false;
+        int repeticionesSimbolo = 0;
+        
+        for (int i = 0; i < arreglo.length; i++) {
+            if(arreglo[i].equals(simbolo)) {
+                repeticionesSimbolo++;
+            }          
+        }
+        
+        repeticionesSimbolo = (repeticionesSimbolo > 1) ? (repeticionesSimbolo - 1) : 0;
+        
+        if(repeticionesSimbolo <= cantMaxRepeticiones)
+            esValidaContrasena = true;
+        
+        return esValidaContrasena;
+    }
+    
+    private static String getSimbolo(int desde, int hasta) {
+        String[] simbolos = {"a","e","i","o","u","b","c","d","f","g","0","1","2","3","4","_","*","+","%"};
+        
+        int posiAletoria = getAleatorio(desde, hasta);
+        
+        return simbolos[posiAletoria];
+    }
+    
+    private static int getAleatorio(int idesde, int ihasta) {
+        int aleatorio = (new Random()).nextInt((ihasta - idesde) + 1);
+        int numeroEnRango = idesde + aleatorio;
+
+        return numeroEnRango;
     }
     
     private static int getNumeroMasRepetido(int[] nums) {
