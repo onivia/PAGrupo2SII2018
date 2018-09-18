a1b475ef4635d2cb221523964c907e78c9a221b5
onivia <ohnivia@gmail.com>
2018-09-17 19:50:34 -0500

    ejemplo4 structs.

--- 6pm_17sept2018_1/main.cpp
+++ 6pm_17sept2018_1/main.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-void f1(int a) {
-    cout<<"paso x f1: "<<a<<endl;
-}
-
-void f2(int b) {
-    cout<<"paso x f2: "<<b<<endl;
-}
-
-void wrapper(void (*f)(int),int i) {
-    f(i);    
-}
+struct Maleta {
+    int cod;
+    double precio;
+    char descripcion[32];    
+};
 
 int main() {
-    void(*p1[])(int) = {f1,f2};
+    Maleta *maletas = (Maleta *)malloc(2*sizeof(Maleta));
     
-    wrapper(p1[0],200);
+    maletas[0].cod = 200;
+    maletas[0].precio = 550.0;
+    strcpy(maletas[0].descripcion,"maleta peppa pig");
     
+    maletas[1].cod = 300;
+    maletas[1].precio = 600.0;
+    strcpy(maletas[1].descripcion,"maleta negra");
+    
+    for(int i=0;i<2;i++) {
+        cout<<maletas[i].cod<<endl;
+        cout<<maletas[i].precio<<endl;
+        cout<<maletas[i].descripcion<<endl;
+    }
+    
+    free(maletas);
+    maletas = NULL;
+        
     return 0;
 }
